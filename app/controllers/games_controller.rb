e0b18b13aac9d053f983a37f1e820d1c43e261a8
matthewljiang <matthewljiang@gmail.com>
2015-06-26 13:32:45 -0400

    Fix errors after removing elo change

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -21,9 +21,7 @@ class GamesController < ApplicationController
       @p1.update_weight()
       @p2.update_weight()
       p1_change = @p1.update_elo(p2_temp_elo, 1)
-      @p1.update_change(p1_change)
       p2_change = @p2.update_elo(p1_temp_elo, 0)
-      @p2.update_change(p2_change)
       @game.update_attribute(:p1_change, p1_change)
       @game.update_attribute(:p2_change, p2_change)
       flash[:notice] = "Game added"
@@ -34,32 +32,12 @@ class GamesController < ApplicationController
   end
 
 
-  # def add
-  #   p1 = Player.find(params[:p1_id])
-  #   p2 = Player.find(params[:p2_id])
-  #   p1_temp_elo = p1.get_elo()
-  #   p2_temp_elo = p2.get_elo()
-  #   p1.update_weight()
-  #   p2.update_weight()
-  #   p1_change = p1.update_elo(p2_temp_elo, 1)
-  #   p1.update_change(p1_change)
-  #   p2_change = p2.update_elo(p1_temp_elo, 0)
-  #   p2.update_change(p2_change)
-  #   Game.create(:winner_id => params[:p1_id], :loser_id => params[:p2_id], 
-  #     :winner_score => params[:wscore], :loser_score => params[:lscore], 
-  #     :challenger_id => params[:ch_id], :p1_change => p1_change, :p2_change => p2_change)
-  #   flash[:notice] = "Game added"
-  #   redirect_to :action => 'index'
-  # end
-
   def destroy
     @game = Game.find(params[:id])
   	p1 = Player.find(@game.winner_id)
     p2 = Player.find(@game.loser_id)
     p1.update_attribute(:elo, p1.get_elo() - @game.p1_change)
-    p1.update_change(-1 * @game.p1_change)
     p2.update_attribute(:elo, p2.get_elo() - @game.p2_change)
-    p2.update_change(-1 * @game.p2_change)
     @game.destroy()
     flash[:notice] = "Game deleted"
   	redirect_to :action => 'index'
